043b2f73ae976fcac0bdfc8fc72ba1440e2cd361
DH <d.hirt@indivikar.ch>
2021-02-05 12:09:06 +0100

    v0.0.1 - first commit

---
+++ src/module-info.java
@@ -0,0 +1,23 @@
+module PDFViewDemo {
+    requires transitive javafx.controls;
+
+    exports app;
+    
+    opens app to javafx.graphics;
+    
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.materialdesign;
+
+    requires java.logging;
+    requires jdk.compiler;
+    
+    requires org.apache.pdfbox;
+    requires org.apache.commons.lang3;
+    requires java.desktop;
+    requires javafx.swing;
+    requires org.controlsfx.controls;
+    
+    requires com.dlsc.pdfviewfx;
+	
+}
